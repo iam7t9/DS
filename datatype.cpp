846401de4a21b350e66661f2c81235cf1c6855b6
iam7t9 <iam7t9@proton.me>
2024-01-09 23:00:12 +0530

    First commit

---
+++ datatype.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int a;
+    float b;
+    char c;
+    bool d;
+
+    // cout << "size of int, float, char, bool are respectively " << sizeof(a) << sizeof(b) << sizeof(c) << sizeof(d) << endl;
+    cout << "int : " << sizeof(a) << endl;
+    cout << "float : " << sizeof(b) << endl;
+    cout << "char : " << sizeof(c) << endl;
+    cout << "bool : " << sizeof(d) << endl;
+    
+    return 0;
+}
