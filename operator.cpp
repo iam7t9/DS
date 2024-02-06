2206a4916baf25fb6bac0082d0b14954909703b7
Shantanu Pande <s.s.pande001@gmail.com>
2024-02-06 15:54:04 +0530

    commited practical code

--- operator.cpp
+++ operator.cpp
@@ -16,3 +16,34 @@ int main()
     
     return 0;
 }
+
+
+
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Enter two numbers: "<< endl;
+    int a,b;
+    
+    cin>>a>>b;
+    
+    int c,d,e;
+    float f;
+    
+    c=a+b;
+    d=a-b;
+    e=a*b;
+    f=(float)a/b;
+    
+    cout << "a + b = " << c << endl;
+    cout << "a - b = " << d << endl;
+    cout << "a / b = " << f << endl;
+    cout << "a * b = " << e << endl;
+    
+    
+    return 0;
+}
+
--- wlcome.cpp
+++ wlcome.cpp
@@ -5,5 +5,17 @@ using namespace std;
 int main()
 {
     cout<< "Welcome to C++ Environment." << endl;
+    int a,b;
+    cout<<"Enter first number";
+    cin>>a;
+    cout<<"Enter second number";
+    cin>>b;
+    if(a<=5 && b<=5){
+        cout<<"a and b are less than 5";
+    }
+    if(a<=5 || b<=5){
+        cout<<"either a or b are less than 5";
+    }
+    
     return 0;
 }
